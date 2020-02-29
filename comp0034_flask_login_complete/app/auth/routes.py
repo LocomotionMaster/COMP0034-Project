ab7d23408f970b978015af956b462a962485c794
Ryan Joseph Sulit Velasquez <47156046+zcecrjs@users.noreply.github.com>
2020-02-29 12:52:49 +0000

    Base for COMP0034 project
    Base for the project, built upon Lecture 6 completed solution

---
+++ comp0034_flask_login_complete/app/auth/routes.py
@@ -0,0 +1,116 @@
+from datetime import timedelta
+from urllib.parse import urlparse, urljoin
+
+from flask import render_template, Blueprint, request, flash, redirect, url_for, make_response, abort
+from flask_login import login_user, login_required, logout_user
+from sqlalchemy.exc import IntegrityError
+
+from app import db, login_manager
+from app.auth.forms import SignupForm, LoginForm
+from app.models import Student, Teacher, User
+
+from sqlalchemy import or_
+from sqlalchemy.orm import with_polymorphic
+
+from app import db
+
+bp_auth = Blueprint('auth', __name__)
+
+
+def is_safe_url(target):
+    host_url = urlparse(request.host_url)
+    redirect_url = urlparse(urljoin(request.host_url, target))
+    return redirect_url.scheme in ('http', 'https') and host_url.netloc == redirect_url.netloc
+
+
+def get_safe_redirect():
+    url = request.args.get('next')
+    if url and is_safe_url(url):
+        return url
+
+    url = request.referrer
+    if url and is_safe_url(url):
+        return url
+    return '/'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Check if user is logged-in on every page load."""
+    if user_id is not None:
+        return User.query.get(user_id)
+    return None
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    """Redirect unauthorized users to Login page."""
+    flash('You must be logged in to view that page.')
+    return redirect(url_for('auth.login'))
+
+
+@bp_auth.route('/signup/', methods=['POST', 'GET'])
+def signup():
+    form = SignupForm(request.form)
+    if request.method == 'POST' and form.validate():
+        if form.role.data == "student":
+            user = Student(name=form.name.data, email=form.email.data, student_ref=form.uni_id.data)
+        else:
+            user = Teacher(name=form.name.data, title=form.title.data, teacher_ref=form.uni_id.data,
+                           email=form.email.data)
+        user.set_password(form.password.data)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            response = make_response(redirect(url_for('main.index')))
+            response.set_cookie("name", form.name.data)
+            return response
+        except IntegrityError:
+            db.session.rollback()
+            flash('ERROR! Unable to register {}. Please check your details are correct and resubmit'.format(
+                form.email.data), 'error')
+    return render_template('signup.html', form=form)
+
+
+@bp_auth.route('/login/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST' and form.validate():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('auth.login'))
+        login_user(user, remember=form.remember_me.data, duration=timedelta(minutes=1))
+        flash('Logged in successfully. {}'.format(user.name))
+        next = request.args.get('next')
+        if not is_safe_url(next):
+            return abort(400)
+        return redirect(next or url_for('main.index'))
+    return render_template('login.html', form=form)
+
+
+@bp_auth.route('/search', methods=['POST', 'GET'])
+@login_required
+def search():
+    if request.method == 'POST':
+        term = request.form['search_term']
+        if term == "":
+            flash("Enter a name to search for")
+            return redirect('/')
+        users = with_polymorphic(User, [Student, Teacher])
+        results = db.session.query(users).filter(
+            or_(users.Student.name.contains(term), users.Teacher.name.contains(term))).all()
+        # results = Student.query.filter(Student.email.contains(term)).all()
+        if not results:
+            flash("No students found with that name.")
+            return redirect('/')
+        return render_template('search_results.html', results=results)
+    else:
+        return redirect(url_for('main.index'))
+
+@bp_auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
